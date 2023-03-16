9c0a41f0989e0720c3382409e88bdecfc35b1136
lillyreeth <lillyreeth.sali42@gmail.com>
2023-03-16 19:16:31 +0000

    Modified webtables2 class file

---
+++ selenium/JavaProject_WebDriver/src/packageWebdriver/StringBufferEx.java
@@ -0,0 +1,21 @@
+package packageWebdriver;
+
+public class StringBufferEx {
+
+	public static void main(String[] args) 
+	{
+		
+		
+      String s1="Lilly";
+      StringBuffer sb=new StringBuffer("Test String");
+      System.out.println(s1);
+      System.out.println(s1.concat(" Reethu"));
+      System.out.println(sb.append(" example"));
+      
+     
+      
+		
+		
+	}
+
+}
