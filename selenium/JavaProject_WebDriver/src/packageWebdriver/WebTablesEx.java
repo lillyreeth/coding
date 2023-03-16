46cc1c215e96095eb7ed643bf6354671652c3db6
lillyreeth <lillyreeth.sali42@gmail.com>
2023-03-16 14:54:53 +0000

    Added ne files

--- selenium/JavaProject_WebDriver/src/packageWebdriver/WebTablesEx.java
+++ selenium/JavaProject_WebDriver/src/packageWebdriver/WebTablesEx.java
@@ -1,7 +1,13 @@
 package packageWebdriver;
 
+
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebTablesEx {
 
@@ -10,20 +16,49 @@ public class WebTablesEx {
 		
 		System.setProperty("Webdriver.chrome.driver", "/JavaProject_WebDriver/External_Files");
 
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		//WebDriver driver = new ChromeDriver();
 		
-      
+    	ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+     	WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://www.w3schools.com/html/html_tables.asp");
+		driver.findElement(By.id("accept-choices")).click();
+		
+		System.out.println("before table assignment");
+		
+		WebElement table=driver.findElement(By.xpath("//table[@id='customers']"));
+		
+		// This method getText() gives complete table data which looks ugly
+		//System.out.println(table.getText());
+		
+		System.out.println("before list assignment");
+		List <WebElement> table_header=table.findElements(By.xpath("//div[@class='w3-example']//tr[1]//th"));
+		
+		System.out.println("after list assignment");
+		
+		System.out.println(table_header.size());
+		//System.out.println(table_header.get(1).getText());
+		//Finding the table header row count and printing the web elements
+		for(int i=0;i<table_header.size();i++)
+		{
+			System.out.println(table_header.get(i).getText());
+		}
+		
+		List <WebElement> table_row1=table.findElements(By.xpath("//div[@class='w3-example']//tr[2]//td"));
+		for(int i=0;i<table_row1.size();i++)
+		{
+		System.out.println(table_row1.get(i).getText());
+		}
 		
 		
 		try {
-			 driver.get("https://www.w3schools.com/html/html_tables.asp");
-      Thread.sleep(2000);
-      driver.quit();
-      }
-		catch(Exception e)
-		{
+
+			Thread.sleep(2000);
+			driver.quit();
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
+			driver.quit();
 		}
 		
 	}
---
+++ selenium/JavaProject_WebDriver/src/packageWebdriver/WebTablesEx2.java
@@ -0,0 +1,68 @@
+package packageWebdriver;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebTablesEx2 {
+
+	public static void main(String[] args) 
+	{
+		
+		System.setProperty("Webdriver.chrome.driver", "/JavaProject_WebDriver/External_Files");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		options.addArguments("--headless");
+
+     	WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://www.w3schools.com/html/html_tables.asp");
+		System.out.println(driver.getTitle());
+		//driver.findElement(By.id("accept-choices")).click();
+		
+		WebElement table=driver.findElement(By.xpath("//table[@id='customers']"));
+		List <WebElement> all_rows=table.findElements(By.xpath("//div[@class='w3-example']//tr"));
+		System.out.println(all_rows.size());
+		
+		List <WebElement> table_header_row1=table.findElements(By.xpath("//div[@class='w3-example']//tr[1]//th"));
+		for(int j=0;j<table_header_row1.size();j++)
+		{
+		System.out.print(table_header_row1.get(j).getText() + " ");
+		}
+		
+		 
+	   for(int k=2;k<=all_rows.size();k++)
+	   {
+		
+		List <WebElement> table_data_row=table.findElements(By.xpath("//div[@class='w3-example']//tr[" + k + "]//td"));
+		System.out.println(" ");
+	    
+		String compareString = "Laughing Bacchus Winecellars";
+		if(table_data_row.get(0).getText().contains(compareString)) 
+		for(int i=0;i<table_data_row.size();i++)
+	    {
+	   
+	    		System.out.print(table_data_row.get(i).getText()+ " ");	
+	    }
+	   }
+	 
+	   
+	   
+	    driver.quit();
+		 
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
